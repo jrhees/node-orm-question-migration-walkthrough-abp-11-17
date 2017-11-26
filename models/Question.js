69657b9af0351f031413170584672ab31cb1eccf
Jennifer Rhees <redswinglinestapler23@gmail.com>
2017-11-26 23:19:59 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -4,7 +4,12 @@ const db = require("../config/db")
 
 class Question{
   static CreateTable() {
-
+    return new Promise(function(resolve)) {
+      const sql = `CREATE TABLE questions (id INTEGER PRIMARY KEY)`
+      db.run(sql, function() {
+        resolve("questions table created")
+      })
+    }
   }
 }
 
